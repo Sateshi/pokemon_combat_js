02c9a32b024edc543dd02f69a027e44da8016eca
Sateshi <Sateshiplay@gmail.com>
2021-12-06 20:31:47 +0100

    sprites and hp bar

--- public/js/combat.js
+++ public/js/combat.js
@@ -1,7 +1,23 @@
+function pvVisuel(pokemon1,pokemon2,pv1max,pv2max){
+    var elem = document.getElementById("myBar");
+    var elem2 = document.getElementById("myBar2");
+    var width1 = (pokemon1.vie/pv1max) * 100 + "%";
+    var width2 = (pokemon2.vie/pv2max) * 100 + "%";
+    if(width1 < 0)
+        width1 = 1 + "%";
+    if(width2 < 0)
+        width2 = 1 + "%";
+    elem.style.width = width1;
+    elem2.style.width = width2;
+}
+
 export function startCombat(pokemon1, pokemon2) {
     let choix;
     let i = 1;
+    let pv1max = pokemon1.vie;
+    let pv2max = pokemon2.vie;
     while (pokemon1.vie > 0 && pokemon2.vie > 0) {
+        pvVisuel(pokemon1,pokemon2,pv1max,pv2max);
         choix = +prompt(`Tour ${i} : \nPoints de vie de votre ${pokemon1.nom} : ${pokemon1.vie}\n
         Points de vie du ${pokemon2.nom} ennemi : ${pokemon2.vie}\n
         Quel attaque voulez-vous choisir : \n
@@ -23,7 +39,7 @@ export function startCombat(pokemon1, pokemon2) {
                     break;
             }
             if(pokemon2.vie > 0){
-                switch (Math.floor(Math.random() * (4 - 1 + 1) + 1)) {
+                switch (Math.floor(Math.random() * 4 + 1)) {
                     case 1:
                         pokemon2.flammeche(pokemon1);
                         break;
@@ -71,9 +87,14 @@ export function startCombat(pokemon1, pokemon2) {
                 }
             }
         }
+        pvVisuel(pokemon1,pokemon2,pv1max,pv2max);
     }
-    if (pokemon1.vie > 0)
+    if (pokemon1.vie > 0){
         alert("VICTOIRE");
-    else
+        document.getElementById("myBar2").style.width = "0%"
+    }
+    else{
         alert("Defaite");
+        document.getElementById("myBar").style.width = "0%"
+    }
 }
